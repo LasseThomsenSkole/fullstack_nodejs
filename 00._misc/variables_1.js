853a8b3d64a6cb3e44da64ca991dfb1c5c551774
LasseThomsenSkole <lath0003@stud.kea.dk>
2025-01-31 10:39:58 +0100

    innit

---
+++ 00._misc/variables_1.js
@@ -0,0 +1,12 @@
+const hej = 2;
+//hej = 3 can't do this
+//hej += 3 can't do this
+
+const me = {};
+me.name = 'bombo';
+console.log(me)
+
+const array = [1,2,3];
+console.log("her er en array", array)
+array.push(4);
+console.log("her er samme array", array)
