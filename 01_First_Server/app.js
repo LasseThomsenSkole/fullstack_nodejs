efce5c3e1c76e8dfd737a838cbf6f1a823d48e16
LasseThomsenSkole <lath0003@stud.kea.dk>
2025-02-14 11:12:29 +0100

    search endpoint

--- 01_First_Server/app.js
+++ 01_First_Server/app.js
@@ -5,16 +5,21 @@ app.get("/", (req, res)=>{
     res.send({data: "hello world"});
 });
 
+app.get("/search", (req, res)=>{
+    res.send({data: `you have searched for: ${req.query.q}`});
+});
+
 app.get("/blablabla", (req, res)=>{
     res.send({data: "bla bla bla"});
 });
+
 app.get("/yourfavoritenumber/:favoriteNumber", (req, res) =>{
     const number= req.params.favoriteNumber;
     res.send({data: number})
-})
+});
 
 app.get("/favoritethings/:animal/:flower", (req,res) =>{
     res.send({data:`${req.params.flower} & ${req.params.animal}`});
-})
+});
 
 app.listen(80);
