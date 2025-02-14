04b6cfd9c35aa242c4f0ea4579212eac87044427
LasseThomsenSkole <lath0003@stud.kea.dk>
2025-02-14 11:45:45 +0100

    html

--- 01_First_Server/app.js
+++ 01_First_Server/app.js
@@ -1,5 +1,6 @@
 const express=  require('express');
 const app = express();
+app.use(express.json())
 
 app.get("/", (req, res)=>{
     res.send({data: "hello world"});
@@ -16,10 +17,23 @@ app.get("/blablabla", (req, res)=>{
 app.get("/yourfavoritenumber/:favoriteNumber", (req, res) =>{
     const number= req.params.favoriteNumber;
     res.send({data: number})
-});
+})
 
 app.get("/favoritethings/:animal/:flower", (req,res) =>{
-    res.send({data:`${req.params.flower} & ${req.params.animal}`});
-});
+    res.send({data:`${req.params.flower} & ${req.params.animal}`})
+})
+app.get("/welcomepage", (req, res)=>{
+    res.sendFile(__dirname +'/index.html')
+})
+
+app.post("/favoritepolitiker", (req, res)=>{
+    console.log(req.body)
+    res.send({data: req.body})
+})
+
+app.post("/send", (req, res)=>{
+    const dataObject = req.body
+    res.send({data: dataObject})
+})
 
 app.listen(80);
