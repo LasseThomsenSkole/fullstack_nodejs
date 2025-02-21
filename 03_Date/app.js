993b84cf9a34ca5cce0bff64af351ad8ebb078c5
LasseThomsenSkole <lath0003@stud.kea.dk>
2025-02-21 11:04:09 +0100

    removed unused plugin

--- 03_Date/app.js
+++ 03_Date/app.js
@@ -1,8 +1,6 @@
 const express = require("express")
 const app = express()
 
-app.use(express.json())
-
 console.log(new Date()) // UTC
 console.log(Date()) // local time zone / OS time zone
 const months = [
