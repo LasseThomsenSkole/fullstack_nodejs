08334d71b0eb4dd16fa1ba30f595e56cd0ff90c5
LasseThomsenSkole <lath0003@stud.kea.dk>
2025-02-21 11:45:01 +0100

    added visitor count

--- 04_HTML_server/app.js
+++ 04_HTML_server/app.js
@@ -1,11 +1,16 @@
 const express = require("express")
 
 const app = express()
+let visitorCount= 0;
 
 app.get("/", (req, res) =>{
     res.sendFile(__dirname + "/public/frontpage.html")
 })
 
+app.get("/visitorcounts", (req, res)=>{
+    visitorCount++
+    res.send({data: visitorCount})
+})
 
 const PORT = 80
 app.listen(PORT)
