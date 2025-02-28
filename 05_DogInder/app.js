ce879fdc70c07a40dfde9712ed0cfb3980a89855
LasseThomsenSkole <lath0003@stud.kea.dk>
2025-02-28 11:44:47 +0100

    notes

--- 05_DogInder/app.js
+++ 05_DogInder/app.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
-
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve('public/frontpage/frontpage.html'));
+});
 const PORT = 80;
 app.listen(PORT);
