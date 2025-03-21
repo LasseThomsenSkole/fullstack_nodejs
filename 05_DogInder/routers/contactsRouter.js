e7d1bd25455f7eb5a326be020710b26b72bff3f5
LasseThomsenSkole <lath0003@stud.kea.dk>
2025-03-21 09:53:06 +0100

    added contact api endpoint

--- 05_DogInder/routers/contactsRouter.js
+++ 05_DogInder/routers/contactsRouter.js
@@ -3,7 +3,8 @@ import { Router } from 'express'
 const router = Router()
 
 router.post("/api/contact", (req, res) =>{
-    res.send({data: req.body})
+    console.log(req.body)
+    res.redirect("/")
 })
 
 export default router
