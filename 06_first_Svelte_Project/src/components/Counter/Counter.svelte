e3f3f630ba64ff82823970c3686daf09cf1e323e
LasseThomsenSkole <lath0003@stud.kea.dk>
2025-03-21 11:07:22 +0100

    added counter example

---
+++ 06_first_Svelte_Project/src/components/Counter/Counter.svelte
@@ -0,0 +1,14 @@
+<script>
+    let counter = $state(0);
+
+</script>
+
+<h3>
+    counter {counter}
+</h3>
+<button onclick={() => counter++}>
+    increment
+</button>
+<button onclick={() => counter--}>
+    decrement
+</button>
---
+++ 06_first_Svelte_Project/src/components/Messages/Messages.svelte
@@ -0,0 +1,13 @@
+<script>
+    const messages = $state([",21,312", 2,32,344])
+    let messageInput = $state("");
+    function addMessage(){
+        messages.push(messageInput)
+        messageInput = ""
+    }
+</script>
+
+<h1>Messages</h1>
+<p>{messages}</p>
+<input bind:value={messageInput} placeholder="type message">
+<button onclick={addMessage}>submit message</button>
