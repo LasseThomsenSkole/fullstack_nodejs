f2b2eaf5728c38269898ab76426925960fa4224a
LasseThomsenSkole <lath0003@stud.kea.dk>
2025-03-21 11:45:27 +0100

    first message

---
+++ 06_first_Svelte_Project/src/components/Messages/FirstMessage.svelte
@@ -0,0 +1,4 @@
+<script>
+    const {firstMessage = "no messages"} = $props()
+</script>
+<h1>first message: {firstMessage}</h1>
--- 06_first_Svelte_Project/src/components/Messages/Messages.svelte
+++ 06_first_Svelte_Project/src/components/Messages/Messages.svelte
@@ -1,12 +1,14 @@
 <script>
-    const messages = $state([",21,312", 2,32,344])
+    import FirstMessage from "./FirstMessage.svelte";
+
+    const messages = $state([])
     let messageInput = $state("");
     function addMessage(){
         messages.push(messageInput)
         messageInput = ""
     }
 </script>
-
+<FirstMessage firstMessage={messages[0]}/>
 <h1>Messages</h1>
 <p>{messages}</p>
 <input bind:value={messageInput} placeholder="type message">
