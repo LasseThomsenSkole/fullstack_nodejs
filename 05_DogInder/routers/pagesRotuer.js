42b2f5dfff5930570056b91e62383a3f2e9bab62
LasseThomsenSkole <lath0003@stud.kea.dk>
2025-03-21 08:35:01 +0100

    added contact page

--- 05_DogInder/routers/pagesRotuer.js
+++ 05_DogInder/routers/pagesRotuer.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 // den skal init
 const router = Router();
 
-import {frontpagePage, matchesPage} from "../util/pages.js";
+import {frontpagePage, matchesPage, contactPage} from "../util/pages.js";
 
 router.get("/", (req, res) => {
     res.send(frontpagePage);
@@ -11,5 +11,8 @@ router.get("/", (req, res) => {
 router.get("/matches", (req, res) => {
     res.send(matchesPage);
 });
+router.get("/contact", (req, res) => {
+    res.send(contactPage);
+})
 
 export default router;
--- 05_DogInder/util/pages.js
+++ 05_DogInder/util/pages.js
@@ -1,11 +1,17 @@
-import { readPage, constructPage } from "./templatingEngine.js";
+import {readPage, constructPage} from "./templatingEngine.js";
 
 const frontpage = readPage('./public/pages/frontpage/frontpage.html')
 const matches = readPage('./public/pages/matches/matches.html')
+const contact = readPage('./public/pages/contact/contact.html')
 
-export const frontpagePage =constructPage(frontpage)
+export const frontpagePage = constructPage(frontpage)
 export const matchesPage = constructPage(matches,
-{
-    title: 'DogInder | Matches',
-    cssLinks: '<link rel="stylesheet" href="/pages/matches/matches.css">'
-});
+    {
+        title: 'DogInder | Matches',
+        cssLinks: '<link rel="stylesheet" href="/pages/matches/matches.css">'
+    });
+export const contactPage = constructPage(contact, {
+        title: 'DogInder | contact',
+        cssLinks: '<link rel="stylesheet" href="/pages/contact/contact.css">'
+    }
+)
