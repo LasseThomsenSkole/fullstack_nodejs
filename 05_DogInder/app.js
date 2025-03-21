349ceb1b3a95587c07d57f93b830cac77e2100c5
LasseThomsenSkole <lath0003@stud.kea.dk>
2025-03-21 09:25:18 +0100

    added contact api endpoint

--- 05_DogInder/app.js
+++ 05_DogInder/app.js
@@ -1,13 +1,15 @@
 import express from 'express';
+import pagesRouter from './routers/pagesRotuer.js'
+import matchesRouter from './routers/matchesRouter.js'
+import contactsRouter from "./routers/contactsRouter.js";
 
 const app = express();
 
 app.use(express.static("public"));
-import pagesRouter from './routers/pagesRotuer.js'
-import matchesRouter from './routers/matchesRouter.js'
-
+app.use(express.urlencoded({extended: true}))
 app.use(pagesRouter);
 app.use(matchesRouter);
+app.use(contactsRouter)
 
 const PORT = Number(process.env.PORT) || 8080;
 const server = app.listen(PORT, () => console.log("Server is running on port", server.address().port));
