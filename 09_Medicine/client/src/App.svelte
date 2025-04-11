01b51e56545a1a3530080c579eef6e0d0460ef60
LasseThomsenSkole <lath0003@stud.kea.dk>
2025-04-11 09:17:23 +0200

    added routes

---
+++ 09_Medicine/client/src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+import { Router, Link, Route } from "svelte-routing";
+import Home from "./pages/Home/Home.svelte";
+import About from "./pages/About/About.svelte";
+import Pharmacy from "./pages/Pharmacy/Pharmacy.svelte";
+</script>
+<Router>
+    <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/pharmacy">pharmacy</Link>
+    </nav>
+
+    <div>
+        <Route path="/">
+            <Home/>
+        </Route>
+        <Route path="/about">
+            <About/>
+        </Route>
+        <Route path="/pharmacy">
+            <Pharmacy/>
+        </Route>
+    </div>
+</Router>
