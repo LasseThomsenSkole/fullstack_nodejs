cae2dfebb7acd864fb5a8921f0a2b0196336f7cc
LasseThomsenSkole <lath0003@stud.kea.dk>
2025-04-11 09:42:04 +0200

    added router for pills

---
+++ 09_Medicine/server/routers/pillsRouter.js
@@ -0,0 +1,11 @@
+import {Router} from "express";
+
+const router = Router();
+router.get("/pills", (req, res) => {
+res.send({});
+});
+router.post("/pills", (req, res) => {
+res.send({});
+});
+
+export default router;
