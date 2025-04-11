9d7f55e35d5c3e2aee8abd946f7405d31857cbbb
LasseThomsenSkole <lath0003@stud.kea.dk>
2025-04-11 11:02:21 +0200

    pill functionality

--- 09_Medicine/server/routers/pillsRouter.js
+++ 09_Medicine/server/routers/pillsRouter.js
@@ -2,10 +2,15 @@ import {Router} from "express";
 
 const router = Router();
 router.get("/pills", (req, res) => {
-res.send({});
+    res.send({data: req.session.pills || [] });
 });
 router.post("/pills", (req, res) => {
-res.send({});
+    if(!req.session.pills){
+        req.session.pills = [];
+    }
+    req.session.pills.push(req.body);
+
+    res.send({data: req.session.pills});
 });
 
 export default router;
