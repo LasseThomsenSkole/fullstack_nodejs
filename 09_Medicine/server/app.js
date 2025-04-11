e7a3789e76b3a820c65329d783f5f954ec01505e
LasseThomsenSkole <lath0003@stud.kea.dk>
2025-04-11 10:05:29 +0200

    added cors

--- 09_Medicine/server/app.js
+++ 09_Medicine/server/app.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import cors from 'cors';
+
 import employeesRouter from "./routers/employeesRouter.js";
 import pillsRouter from './routers/pillsRouter.js'
 const app = express();
+
+app.use(cors())
 
 app.use(employeesRouter);
 app.use(pillsRouter);
