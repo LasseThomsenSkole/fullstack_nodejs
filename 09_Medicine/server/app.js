779d30e23acb94d1c341c51f932d7d86649c8403
LasseThomsenSkole <lath0003@stud.kea.dk>
2025-04-11 10:12:08 +0200

    added cors

--- 09_Medicine/server/app.js
+++ 09_Medicine/server/app.js
@@ -4,8 +4,16 @@ import cors from 'cors';
 import employeesRouter from "./routers/employeesRouter.js";
 import pillsRouter from './routers/pillsRouter.js'
 const app = express();
-
-app.use(cors())
+app.use(cors({
+    origin: "https://localhost:5173",
+}))
+/* kunne også gøre sådan her
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+ */
 
 app.use(employeesRouter);
 app.use(pillsRouter);
