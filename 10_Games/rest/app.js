fb6ebbf331e5f74fa2521e6b4f031fb81eff2863
LasseThomsenSkole <lath0003@stud.kea.dk>
2025-04-25 11:01:12 +0200

    added gamesrouter

--- 10_Games/rest/app.js
+++ 10_Games/rest/app.js
@@ -1,4 +1,7 @@
 import express from "express";
+import gamesRoutes from "./router/gamesRouter.js";
 const app = express();
+app.use(express.json());
+app.use(gamesRoutes);
 
 app.listen(80);
---
+++ 10_Games/rest/router/gamesRouter.js
@@ -0,0 +1,23 @@
+import {Router} from 'express';
+import db from '../../database/connection.js';
+const router = Router();
+
+router.get('/api/games', async (req, res) => {
+    const result = await db.all('SELECT * FROM games');
+    console.log(result);
+    res.send({"data": result});
+})
+router.post('/api/games', async (req, res) => {
+    const {title, genre, runtimeId} = req.body;
+    if (!title || !genre || !runtimeId) {
+        return res.status(400).send({"error": "Missing required fields"});
+    }
+    try{
+        const result = await db.run('INSERT INTO games (title, genre, runtime_environment_id) VALUES (?, ?, ?)', [title, genre, runtimeId]);
+        return res.status(200).send({"data": result.lastID});
+    }catch(err){
+        console.error(err);
+        return res.status(500).send({"error": "Internal server error"});
+    }
+})
+export default router;
