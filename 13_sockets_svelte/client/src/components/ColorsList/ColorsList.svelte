5fc77f9ef55522ad1c7fa71aac03db9b62c08e35
LasseThomsenSkole <lath0003@stud.kea.dk>
2025-05-09 10:38:40 +0200

    color list

---
+++ 13_sockets_svelte/client/src/components/ColorsList/ColorsList.svelte
@@ -0,0 +1,6 @@
+<script>
+    import { colors} from "../../store/colorStore.js";
+</script>
+{#each $colors as {nickname, color}}
+    <div style={`background-color: ${color}`}>{nickname}: {color}</div>
+{/each }
--- 13_sockets_svelte/client/src/pages/Colors/Colors.svelte
+++ 13_sockets_svelte/client/src/pages/Colors/Colors.svelte
@@ -1,11 +1,24 @@
 <script>
     import { onMount} from "svelte";
     import io from 'socket.io-client';
+    import { colors } from "../../store/colorStore.js";
+    import ColorsList from "../../components/ColorsList/ColorsList.svelte";
+
+    let color = "#ff0000"
     let socket;
+
     onMount(()=>{
         socket = io("http://localhost:80");
+        socket.on('server-sends-color', (data)=>{
+            document.body.style.backgroundColor = data.color // gør normal ikke det her (DOM manipulation)
+            const nickname = 'lasse'
+            colors.update((colorsArray)=>{
+                colorsArray.push({ color: data.color, nickname })
+                return colorsArray;
+            })
+        })
     })
-    let color = "#ff0000"
+
     function submitColor(){
         console.log(color)
         socket.emit('client-sends-color', {color})
@@ -13,3 +26,4 @@
 </script>
 <input type="color" bind:value={color}>
 <button onclick={submitColor}>Submit color</button>
+<ColorsList/>
