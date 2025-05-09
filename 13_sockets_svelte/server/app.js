ac7264f32c3ebc52f9038239053c038c4b25404a
LasseThomsenSkole <lath0003@stud.kea.dk>
2025-05-09 10:01:17 +0200

    server init

---
+++ 13_sockets_svelte/server/app.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
+
+const app = express();
+app.use(express.static('public'));
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+const port = process.env.PORT || 80;
+
+server.listen(port, ()=> console.log(`Listening on ${port}`));
+io.on('connection', (socket) => {
+    console.log(`Client connected: ${socket.id}`);
+
+    socket.on('client-sends-color', (data) => {
+        //console.log(data);
+        //socket.emit('server-sends-color', data); virker ik
+        //socket.broadcast.emit('server-sends-color', data); broadcaster til alle andre men ik sig selv
+        io.emit('server-sends-color', data);
+    })
+
+    socket.on('disconnect', () => {
+        console.log(`Client disconnected: ${socket.id}`);
+    })
+})
