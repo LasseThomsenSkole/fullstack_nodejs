9e5b2aa240c466c32d169670364c5ffbdcb3fdc8
LasseThomsenSkole <lath0003@stud.kea.dk>
2025-05-09 10:20:52 +0200

    added cors

--- 13_sockets_svelte/server/app.js
+++ 13_sockets_svelte/server/app.js
@@ -7,7 +7,11 @@ const app = express();
 app.use(express.static('public'));
 
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server,{
+    cors: {
+        origin: '*',
+    }
+});
 
 const port = process.env.PORT || 80;
 
@@ -15,7 +19,7 @@ io.on('connection', (socket) => {
     console.log(`Client connected: ${socket.id}`);
 
     socket.on('client-sends-color', (data) => {
-        //console.log(data);
+        console.log(data);
         //socket.emit('server-sends-color', data); virker ik
         //socket.broadcast.emit('server-sends-color', data); broadcaster til alle andre men ik sig selv
         io.emit('server-sends-color', data);
